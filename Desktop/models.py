3d1476ed59c2acaba76f697a16bd6b5d8f61d9c0
paslo22 <paslo22@gmail.com>
2013-06-19 17:01:38 -0300

    Comenzando a trabajar con los modelos

--- Desktop/models.py
+++ Desktop/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 class Persona(models.Model):
-	nombre=models.CharField(max_length=30)
-	apellido=models.CharField(max_length=30)
+	nombre = models.CharField(max_length=30)
+	apellido = models.CharField(max_length=30)
+	direccion = models.CharField(max_length=30)
 
+class Reo(models.Model):
+	fecha_ingreso = models.DateField()
 
-# Create your models here.
+class Antecedentes(models.Model):
+	Antecedente = models.TextField()
+	reo = models.ForeignKey('Reo')
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ARR.settings")
 
-from Desktop import models
+from Desktop.models import *
 
-p=Persona()
-p.nombre=raw_input('ingrese nombre')
-p.apellido=raw_input('ingrese apellido')
-p.save()
+# p=Persona()
+# p.nombre=raw_input('ingrese nombre')
+# p.apellido=raw_input('ingrese apellido')
+# p.save()
