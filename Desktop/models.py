3e8daa7cf551f66412702b92462654a72f46301a
paslo22 <paslo22@gmail.com>
2013-06-20 12:26:04 -0300

    Modelo terminado (Creo)

--- Desktop/models.py
+++ Desktop/models.py
@@ -11,11 +11,18 @@ class Reo(Persona):
 	tiempo_condena = models.CharField(max_length=30)
 	fecha_ingreso = models.DateField()
 	id_huella = models.IntegerField()
+	calabozo = models.OneToOneField('Calabozo')
+
+class Traslados(models.Model):
+	reo = models.ForeignKey('Reo')
+	descripcion = models.TextField()
+	fecha = models.DateField()
+	responsable = models.ForeignKey('Efectivo')
 
 class Antecedentes(models.Model):
 	antecedente = models.TextField()
+	condena = models.CharField(max_length=30)
 	reo = models.ForeignKey('Reo')
-	calabozo = models.OneToOneField('Calabozo')
 
 class Calabozo(models.Model):
 	tipo=models.TextField()
@@ -26,6 +33,7 @@ class Policia(Persona):
 	fecha_ingreso = models.DateField()
 	cargo = models.CharField(max_length=50)
 	sueldo = models.FloatField()
+	es_jefe = models.ForeignKey('Policia',related_name='jefe')
 
 class Administrativo(Policia):
 	cant_horas = models.IntegerField()
@@ -34,34 +42,27 @@ class Efectivo(Policia):
 	horario_patrull = models.CharField(max_length=30)
 
 class Equipamiento(models.Model):
-	estado=models.CharField(max_length=30)
+	nombre = models.CharField(max_length=30)
+	estado = models.CharField(max_length=30)
+
+class Asignaciones(models.Model):
+	equipamiento = models.ForeignKey('Equipamiento')
+	policia = models.ForeignKey('Policia',related_name='policia')
+	asignado_por = models.ForeignKey('Policia',related_name='asignador')
+	fecha_asignacion = models.DateField()
 
 class Articulos(Equipamiento):
 	tipo = models.CharField(max_length=50)
-	asignado = models.ManyToManyField('Administrativo')
 
 class Armamento(Equipamiento):
-	asignado = models.ForeignKey('Efectivo')
+	tipo_municion = models.CharField(max_length=30)
+	codigo = models.CharField(max_length=30)
 
 class Municion(Equipamiento):
-	asignado = models.ForeignKey('Efectivo')
+	tipo = models.CharField(max_length=30)
+	cantidad = models.IntegerField()
 
 class Vehiculo(Equipamiento):
-	asignado = models.ForeignKey('Efectivo')
-
-
-# Debe controlar los reos que esten asignados a dicha seccional. Estos reos, pueden 
-# ingresar, ser  trasladados de celdas dentro de la institucion, ser transferidos a otra comisaria 
-# o carcel, o eventualmente, ser liberados. Estos reos tienen un nombre y apellido, un numero 
-# de documento, una huella dactilar que lo identifica en una base de datos y se relaciona con 
-# un listado de antecedentes, una fecha de ingreso y un tiempo de condena, expresado en dias, 
-# meses o anios (segun corresponda).
-# Si un preso fuera trasladado o transferido (ya sea de celda o de comisaria/carcel), debe 
-# registrarse dicho movimiento, indicando la fecha del traslado, y un comentario que describa 
-# el por que de dicho movimiento, y un responsable interno encargado de la transferencia.
-# Ademas, la comisaria maneja el equipamiento disponible. Este equipamiento, 
-# comprende armamento, municion y coches patrulla. Cada elemento del equipamiento, se 
-# asigna a un efectivo policial (por ejemplo: un policia utiliza cierta arma, que dispara cierto tipo 
-# de municion, o realiza patrullaje con cierto vehiculo acompaniado un numero determinado 
-# de companieros definidos). Cada asignacion, debe tener, por supuesto, un efectivo y un 
-# equipamiento, indicando la fecha de la asignacion, y el encargado que realizo la asignacion.
+	marca = models.CharField(max_length=30)
+	modelo = models.CharField(max_length=30)
+	capacidad = models.IntegerField()
--- main.py
+++ main.py
@@ -2,8 +2,3 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ARR.settings")
 
 from Desktop.models import *
-
-# p=Persona()
-# p.nombre=raw_input('ingrese nombre')
-# p.apellido=raw_input('ingrese apellido')
-# p.save()
