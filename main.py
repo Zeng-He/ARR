09448182ff5db24f95ae322817e0dcacb6036c71
nicomagg <nicolas_maggione_27@hotmail.com>
2013-06-20 16:56:06 -0300

    20/06/13 16:55

--- main.py
+++ main.py
@@ -2,3 +2,33 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ARR.settings")
 
 from Desktop.models import *
+
+def altaAntecedente(**kw):
+	error = False
+	if (("antecedente" in kw)and("condena" in kw)and("reo" in kw)):
+		if (not(isinstance(kw['antecedente'],str))):
+			error=True
+			break
+		elif (not(isinstance(kw['condena'],str))):
+			error=True
+			break
+		else (not(isinstance(kw['reo'],Reo))):
+			error=True
+	else:
+		error=True
+
+	if(error):
+		return False
+
+	a = Antecedentes()
+	try:
+		alta(a,**kw)
+		return(a)
+	except Exception, e:
+		return False
+
+def buscarAntecedentes(idReo):
+	return(Antecedentes.objects.filter(reo_id=idReo))
+
+#def realizarTraslado(**kw):
+#	if (("descripcion" in kw)and("responsable" in kw)and("fecha" in kw)and("reo" in kw)):
