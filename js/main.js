db2a4b0dae0f792131477d32e9288823d637d9c7
nicomagg <nicolas_maggione_27@hotmail.com>
2013-06-24 01:49:42 -0300

    24/06/13 01:49

--- js/main.js
+++ js/main.js
@@ -1,33 +1,63 @@
 $(document).ready(function() {
-	$('#cal').click(function() {
-		t=$('#inputCalT').val()
-		e=$('#inputCalE').val()
-		PyAsync('altaCalabozoInterf',{
-			args:[t,e],
-			callback: function(data) {
-				alert(data)
-			}
-		})
+	
+	//-----------REOS---------------
+	$("#reos").click(function(e){
+		e.preventDefault();
+		$("#infoInicio").css({'display':'none'});
+		$("#policias").attr('class','');
+		$("#asignacionActiviades").attr('class','');
+		$("#traslado").attr('class','');
+		$("#equipamiento").attr('class','');
+		$("#asignacionEquipamiento").attr('class','');
+		$("#reos").attr('class','active');
+		$("#reoInicio").css({'display':'inline'})
 	})
 
-	/*$('#cCal').click(function() {
-		PyAsync('test_calabozo',{
-			args:['Ladero','Ladero2'],
-			callback:function(data) {
-				alert(data)
-			}
-		})
+	$("#buttonBuscarReo").click(function(e){
+		e.preventDefault();
+		$("#reoInicio").css({'display':'none'});
+		$("#reoBusqueda").css({'display':'inline'});
 	})
-	$('#cCal').click(function() {
-		tipo=$('#inputTipo').val()
-		estado=$('#inputEstado').val()
-		alert(tipo)
-		PyAsinc('test_calabozo',{
-			args:[tipo,estado],
-			callback:function(data) {
-				alert('ladero')
-				$('#cCal').attr('class','btn btn-success disabled')
-			}
-		})
-	})*/
+
+	$("#buttonAltaReo").click(function(e){
+		e.preventDefault();
+		$("#reoInicio").css({'display':'none'});
+		$("#reoAlta").css({'display':'inline'});
+	})	
+
+
+
+
+
+	// $('#cal').click(function() {
+	// 	t=$('#inputCalT').val()
+	// 	e=$('#inputCalE').val()
+	// 	PyAsync('altaCalabozoInterf',{
+	// 		args:[t,e],
+	// 		callback: function(data) {
+	// 			alert(data)
+	// 		}
+	// 	})
+	// })
+
+	// $('#cCal').click(function() {
+	// 	PyAsync('test_calabozo',{
+	// 		args:['Ladero','Ladero2'],
+	// 		callback:function(data) {
+	// 			alert(data)
+	// 		}
+	// 	})
+	// })
+	// $('#cCal').click(function() {
+	// 	tipo=$('#inputTipo').val()
+	// 	estado=$('#inputEstado').val()
+	// 	alert(tipo)
+	// 	PyAsinc('test_calabozo',{
+	// 		args:[tipo,estado],
+	// 		callback:function(data) {
+	// 			alert('ladero')
+	// 			$('#cCal').attr('class','btn btn-success disabled')
+	// 		}
+	// 	})
+	// })
 })
